14231e105e6a1f79242c511660f3e1acc8bab1c7
falconwood <66784976+falconwood@users.noreply.github.com>
2020-06-11 12:54:16 -0400

    Add files via upload
    Powershell to enable SSH on Windows node.

---
+++ ConfigureSSHonWindowsForAnsible.ps1
@@ -0,0 +1,6 @@
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Set-Service -Name sshd -StartupType 'Automatic'
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+New-LocalUser -Name "ansible" -FullName "The Ansible" -Password ""
+Add-LocalGroupMember -Group 'Administrators' -Member ('ansible')
